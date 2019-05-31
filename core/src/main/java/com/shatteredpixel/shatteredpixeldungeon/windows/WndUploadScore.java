7ef3fa66b2f66a3a724820eb6790ed269269e680
tjlee <dldlxowns@naver.com>
2019-06-01 00:54:08 +0900

    make window qusetion of score upload

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndUploadScore.java
@@ -0,0 +1,14 @@
+package com.shatteredpixel.shatteredpixeldungeon.windows;
+
+import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
+
+public class WndUploadScore extends Window {
+
+    private static final int WIDTH_P = 120;
+    private static final int WIDTH_L = 144;
+
+    private static final int MARGIN 		= 2;
+    private static final int BUTTON_HEIGHT	= 20;
+
+    public WndQuestion
+}
