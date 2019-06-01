249c6a42468cf956ba030c77d27ac757fcfe6211
tjlee <dldlxowns@naver.com>
2019-06-01 21:38:02 +0900

    make Window for Login

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndLogin.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndLogin.java
@@ -1,22 +1,36 @@
 package com.shatteredpixel.shatteredpixeldungeon.windows;
 
-import android.widget.EditText;
-
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
+import com.shatteredpixel.shatteredpixeldungeon.ui.RedButton;
+import com.shatteredpixel.shatteredpixeldungeon.ui.RenderedTextMultiline;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
 
 public class WndLogin extends Window {
-    private int WIDTH_P = 120;
-    private int WIDTH_L = 131;
+    private static final int WIDTH      = 120;
+    private static final int HEIGHT     = 80;
 
-    private int MIN_HEIGHT = 100;
+    private static final int BTN_HEIGHT = 18;
+    private static final float GAP      = 2;
 
-    private int BTN_WIDTH = 50;
-    private int BTN_HEIGHT = 50;
+    public WndLogin() {
 
-    private EditText textInput;
-    private static final int MAX_LEN = 20;
-
-    public WndLogin(){
         super();
+
+        RenderedTextMultiline form = PixelScene.renderMultiline( 6 );
+        form.text( Messages.get(this, "id") + "\n\n" + "" + "\n\n" + Messages.get(this, "pwd") + "\n\n" + "", WIDTH );
+        add( form );
+
+        RedButton btnSignIn = new RedButton( Messages.get(this, "sign_in"));
+        btnSignIn.setSize(WIDTH/2-GAP, BTN_HEIGHT);
+        btnSignIn.setPos(1, 61);
+        add( btnSignIn );
+
+        RedButton btnSignUp = new RedButton( Messages.get(this, "sign_up"));
+        btnSignUp.setSize(WIDTH/2-GAP, BTN_HEIGHT);
+        btnSignUp.setPos(61, 61);
+        add( btnSignUp );
+
+        resize(WIDTH, HEIGHT);
     }
 }
