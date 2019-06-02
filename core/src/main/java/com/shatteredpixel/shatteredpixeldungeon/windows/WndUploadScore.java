56c54ee550d053f68774ff30e3dd25b779cf5a0c
tjlee <dldlxowns@naver.com>
2019-06-02 23:01:56 +0900

    make upload function

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndUploadScore.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndUploadScore.java
@@ -1,12 +1,18 @@
 package com.shatteredpixel.shatteredpixeldungeon.windows;
 
+import com.shatteredpixel.shatteredpixeldungeon.Rankings;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.PixelScene;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RedButton;
 import com.shatteredpixel.shatteredpixeldungeon.ui.RenderedTextMultiline;
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
 
+import java.util.ArrayList;
+
 public class WndUploadScore extends Window {
+
+    String URL= "http://220.149.14.84/dashboard/index.php";
+
     private static final int WIDTH      = 120;
     private static final int HEIGHT     = 80;
 
@@ -22,7 +28,12 @@ public class WndUploadScore extends Window {
         form.setPos(1, 1);
         add( form );
 
-        RedButton btnYes = new RedButton( Messages.get(this, "upload_yes"));
+        RedButton btnYes = new RedButton( Messages.get(this, "upload_yes")) {
+            protected void onClick() {
+                upload();
+                hide();
+            }
+        };
         btnYes.setSize(WIDTH/2-GAP, BTN_HEIGHT);
         btnYes.setPos(1, 61);
         add( btnYes );
@@ -33,5 +44,14 @@ public class WndUploadScore extends Window {
         add( btnNo );
 
         resize(WIDTH, HEIGHT);
+    }
+
+    private void upload() {
+
+        Rankings.Record rec = new Rankings.Record();
+        ArrayList<Rankings.Record> records;
+        int lastRecord = Rankings.INSTANCE.lastRecord;
+
+
     }
 }
